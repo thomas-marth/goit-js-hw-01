b7b1ee7ad7562f49ea7a1c03d3306c9073a2e481
Dmitriy Bazhenov <dmitriy.bazhenov@gmail.com>
2020-12-13 13:02:41 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,21 +1,20 @@
 const pricePerDroid = 3000;
 let credits = 23580;
 let message;
-let totalPrice;
 
 const CANCELED = 'Отменено пользователем!';
 const INSUFFICIENT_FUNDS = 'Недостаточно средств на счету!';
 const INVALID_PASSWORD = 'Доступ запрещен, неверный пароль!';
 
 let droidsQuantity = prompt('Введите количество дроидов');
+let totalPrice = Number(droidsQuantity) * pricePerDroid;
 
 if (droidsQuantity === null) {
   message = CANCELED;
-} else if (Number(droidsQuantity) * pricePerDroid >= credits) {
+} else if (Number(droidsQuantity) * pricePerDroid > credits) {
   message = INSUFFICIENT_FUNDS;
-  totalPrice = droidsQuantity * pricePerDroid;
 } else {
-  credits = credits - Number(droidsQuantity) * pricePerDroid;
+  credits = credits - totalPrice;
   message = `Вы купили ${droidsQuantity} дроидов, на счету осталось ${credits} кредитов.`;
 }
 
