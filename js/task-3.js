5aa0d8534c5398c624945f49244fc6844c7b1cfb
Dmitriy Bazhenov <dmitriy.bazhenov@gmail.com>
2020-12-13 21:35:53 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -2,13 +2,13 @@ const ADMIN_PASSWORD = 'jqueryismyjam';
 let message;
 
 const CANCELED = 'Отменено пользователем!';
-const SUCCESS = 'Добро пожаловать!';
+const LOGIN_SUCCESS = 'Добро пожаловать!';
 const INVALID_PASSWORD = 'Доступ запрещен, неверный пароль!';
 
 let userPassword = prompt('Введите пароль:');
 
 if (userPassword === ADMIN_PASSWORD) {
-  message = SUCCESS;
+  message = LOGIN_SUCCESS;
 } else if (userPassword === null) {
   message = CANCELED;
 } else {
