d4e083e2c9fe746e852d0590769e6590aaae4723
Dmitriy Bazhenov <dmitriy.bazhenov@gmail.com>
2020-12-13 15:52:19 +0200

    task 5 completed

---
+++ js/task-5.js
@@ -0,0 +1,34 @@
+let cost;
+let destination = prompt('Введите страну доставки:');
+let userValue = destination.toLowerCase();
+
+switch (userValue) {
+  case 'китай':
+    cost = 100;
+    break;
+
+  case 'чили':
+    cost = 250;
+    break;
+
+  case 'австралия':
+    cost = 170;
+    break;
+
+  case 'индия':
+    cost = 170;
+    break;
+
+  case 'ямайка':
+    cost = 170;
+    break;
+
+  default:
+    alert('В вашей стране доставка не доступна');
+}
+
+if (cost > 0) {
+  alert(
+    `Доставка в ${destination.toUpperCase()} будет стоить ${cost} кредитов`,
+  );
+}
